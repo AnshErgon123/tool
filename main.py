337fc08e5a50aef36b001eae6dcbbeba7220e230
AnshErgon123 <ansh.malhotra@ergon-labs.com>
2025-05-25 18:05:04 +0530

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # main.py
+import os
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    socketio.run(app, host="0.0.0.0", port=port)
