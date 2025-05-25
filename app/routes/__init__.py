87d96f7dcd83ae166aedbe778663f7def3f14ac9
AnshErgon123 <ansh.malhotra@ergon-labs.com>
2025-05-25 17:57:43 +0530

    first commit

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,18 @@
+# app/__init__.py
 from flask import Flask
 from flask_socketio import SocketIO
-from .routes.can_monitor import can_monitor_bp
+from dotenv import load_dotenv
+import os
 
 socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
-    app = Flask(__name__, static_folder="static")
+    load_dotenv()
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.getenv("SECRET_TOKEN")
+
+    from app.routes.can_monitor import can_monitor_bp
     app.register_blueprint(can_monitor_bp)
+
+    socketio.init_app(app)
     return app
