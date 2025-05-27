c840c02afee47c26eba8112367fe4edcdfe3a1b1
AnshErgon123 <ansh.malhotra@ergon-labs.com>
2025-05-27 10:43:37 +0530

    data table

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # main.py
 import os
 from app import create_app, socketio
+from flask import render_template
+from app.routes.data_table import bp as data_table_bp
 
 app = create_app()
+app.register_blueprint(data_table_bp)
+
+@app.route('/datatable')
+def datatable():
+    return render_template('datatable.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
